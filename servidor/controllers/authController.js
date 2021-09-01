f74ea69ae9aff87c58983f19f5f90438eeace374
Alejalons <ux.mor.dez@gmail.com>
2021-09-01 12:12:32 -0400

    .

--- servidor/controllers/authController.js
+++ servidor/controllers/authController.js
@@ -4,9 +4,46 @@ const { validationResult } = require('express-validator'); //resultado de la val
 const jwt = require('jsonwebtoken'); //importacion de token
 
 exports.autenticarUsuarios = async (req, res) => {
-      //revisar si hay errores
-      const errores = validationResult(req);
-      if(!errores.isEmpty()){
-          return res.status(400).json({errores: errores.array()})
-      }
+    //revisar si hay errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()})
+    }
+
+    //extrar el email y passw 
+    const { email, password } = req.body;
+
+    try {
+        //revisar que sea un usuario registrado
+        let usuario = await  Usuario.findOne({ email });
+        if(!usuario){
+            return res.status(400).json({msg: 'El usuario no existe!'});
+        }
+
+        //revisar el password
+        const passCorrecto = await bcryptjs.compare(password, usuario.password );
+        if(!passCorrecto){
+            return res.status(400).json({msg: 'contraseña incorrecta'});
+        }
+
+            // si todo es correcto
+        //crear y firmar el JWT
+        const payload= {
+            usuario: {
+                id: usuario.id
+            }
+        };
+        // firmar el jwt
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 //1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            //mensaje de confirmacion
+             res.json({token}) //enviar respuesta emprime en postman
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
 }
