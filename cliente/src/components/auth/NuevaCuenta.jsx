8653cff1c01d522b17e1ab213d80856df566776e
Alejalons <ux.mor.dez@gmail.com>
2021-09-09 22:16:05 -0300

    dsa2

--- cliente/src/components/auth/NuevaCuenta.jsx
+++ cliente/src/components/auth/NuevaCuenta.jsx
@@ -1,7 +1,12 @@
-import React , { useState }from 'react'
+import React , { useContext, useState }from 'react'
 import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
 
 function Login() {
+
+    // extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext
 
     const [usuario, guardarUsuario ] = useState({
         nombre: '',
@@ -24,7 +29,9 @@ function Login() {
         e.preventDefault();
 
         // validar que no haya campos vacios
-
+        if(nombre.trim() === '' || email.trim() === '' || password.trim() === '' || passConfirm.trim() === ''){
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+        }
         // Password minimo de 6 caracteres
 
         // Los 2 password son iguales
@@ -33,10 +40,20 @@ function Login() {
     }
     return (
         <div className="form-usuario">
+
+            { alerta ? 
+                (
+                    <div className={`alerta ${alerta.categoria}`}>
+                        {alerta.msg}
+                    </div>
+                ) 
+                : null
+            }
+
             <div className="contenedor-form sombre-dark">
                 <h1>Obtener una Cuenta</h1>
 
-                <form action="">
+                <form onSubmit={onSubmit}>
 
                     <div className="campo-form">
                         <label htmlFor="nombre">Nombre</label>
